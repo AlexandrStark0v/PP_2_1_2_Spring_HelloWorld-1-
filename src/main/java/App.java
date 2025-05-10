5ac220687fd9be73cd789ab0728b31f812039f27
ФÐAlexandr_Stark0v <alexstarzakamsk@gmail.com>
2025-05-10 22:51:54 +0300

    Добавил класс Cat

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,19 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+//        System.out.println(bean.getMessage());
+
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+//        System.out.println(bean1.getMessage());
+
+        Cat bean3 = (Cat) applicationContext.getBean("cat");
+//        System.out.println(bean3.getMessage());
+
+        Cat bean4 = (Cat) applicationContext.getBean("cat");
+//        System.out.println(bean4.getMessage());
+
+        System.out.println((bean == bean1) + " helloworld");
+        System.out.println((bean3 == bean4) + " cat");
     }
 }
